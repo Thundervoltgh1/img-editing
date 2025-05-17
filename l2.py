cd380e03582d31ec07abaf566e3ef9face3ad3b5
Thundervoltgh1 <eniolusi@gmail.com>
2025-05-17 14:56:24 +0100

    Update l2.py

--- l2.py
+++ l2.py
@@ -27,4 +27,23 @@ G= cv2.GaussianBlur(w,(11,11),sigmaX=0)
 cv2.imshow("Gausssian",G)
 cv2.waitKey(0)
 
+#median blur-used in digital processing preserves edges but removes noise
+m= cv2.medianBlur(w,3)
+cv2.imshow("median",m)
+cv2.waitKey(0)
+
+#bilateral blur - only sharp edges are preserved here
+b= cv2.bilateralFilter(w,3,75,75)
+cv2.imshow("bilateral",b)
+cv2.waitKey(0)
+#src − A Mat object representing the source (input image) for this operation.
+'''dst − A Mat object representing the destination (output image) for this operation.
+d − A variable of the type integer representing the diameter of the pixel neighborhood.
+sigmaColor − A variable of the type integer representing the filter sigma in the color space.
+sigmaSpace − A variable of the type integer representing the filter sigma in the coordinate space.
+borderType − An integer object representing the type of the border used.'''
+j=cv2.imread("ashborn.png")
+borderimg=cv2.copyMakeBorder(j,100,100,100,100,cv2.BORDER_CONSTANT )
+cv2.imshow("border image",borderimg)
+cv2.waitKey(0)
 cv2.destroyAllWindows()  
